560a3575ca07be6bc2a5ff1a7ce6d9d3b4572869
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2023-01-17 17:09:12 +0900

    sparseにblock割り当てする関数追加

--- eigen_training/train.hpp
+++ eigen_training/train.hpp
@@ -1,4 +1,5 @@
 #include <Eigen/Dense>
+#include <Eigen/Sparse>
 #include <Eigen/Geometry>
 #include <iostream>
 #include <fstream>
@@ -16,6 +17,27 @@ void showTypeName(T &&tp)
     std::cout << abi::__cxa_demangle(typeid(decltype(std::forward<T>(tp))).name(), 0, 0, &tmp) << std::endl;
 }
 
+template <typename T>
+void sparseBlockAssignation(Eigen::SparseMatrix<T>& sparse_mat, const size_t &row_location, const size_t &col_location,const Eigen::MatrixXd& assign_mat)
+{
+    const size_t assign_row = assign_mat.rows();
+    const size_t assign_col = assign_mat.cols();
+    const size_t sparse_max_col = sparse_mat.cols();
+    const size_t sparse_max_row = sparse_mat.rows();
+    if ((sparse_max_col < assign_col + col_location) || (sparse_max_row < assign_row + row_location))
+    {
+        throw std::range_error("write block will exceed sparse size!!!");
+    }
+    for (size_t row = row_location; row < row_location + assign_row; row++)
+    {
+        for (size_t col = col_location; col < col_location + assign_col; col++)
+        {
+            assert((sparse_max_col >= assign_col + col_location) || (sparse_max_row >= assign_row + row_location)); // over sparse size
+            sparse_mat.insert(row, col) = assign_mat(row - row_location, col - col_location);
+        }
+    }
+}
+
 void play()
 {
     Eigen::Matrix<double, 12, 12> mat = MatrixXd::Zero(12, 12);
@@ -24,33 +46,19 @@ void play()
     tmp << 1, 1, 1,
         1, 1, 1,
         1, 1, 1;
-    a = -MatrixXd::Identity(3, 3);
+    a = 7 * MatrixXd::Constant(3, 3,1);
     std::cout << "---mat---" << std::endl;
     std::cout << tmp * a << std::endl;
     Eigen::Matrix3d tr = Eigen::Matrix3d::Identity();
     std::cout << tr << std::endl;
     std::cout << tr.array().pow(0) << std::endl;
-
-    int32_t horizon_length = 300;
-    static constexpr double T = 0.01;              // サンプリング周期 (s)
-    static constexpr int_fast64_t start_step = 30; // 30 * T = 0.3秒後に歩き出す
-    static constexpr int_fast64_t cycle_step = 40; // 何サイクル毎に足踏みするか、 cycle_step * T = 周期(s)
-    static constexpr double step_length = 0.3;     // (m)
-    Eigen::VectorXd ret = Eigen::VectorXd::Zero(horizon_length);
-    for (int32_t i = 0; i < horizon_length; i++)
-    {
-        if ((i) > start_step)
-        {
-            if ( ((i) / cycle_step) % 2 == 0)
-            {
-                ret(i, 0) = step_length;
-            }
-            else
-            {
-                ret(i, 0) = -step_length;
-            }
-        }
-        // ret(i, 0) = std::floor((double)(i + cycle_step) / (cycle_step + cycle_step)) - std::floor((double)(i) / (cycle_step + cycle_step)) * 2.0f;
-    }
-    std::cout << ret.transpose() << std::endl;
+    std::cout << " ------ sparse mat   ------" << std::endl;
+    Eigen::SparseMatrix<double> sparse;
+    Eigen::Vector3d vec;
+    vec << 1,2,3;
+    sparse.resize(40,40);
+    sparseBlockAssignation(sparse,2,1,a);
+    sparseBlockAssignation(sparse,36,39,vec);
+    sparseBlockAssignation(sparse,39,37,vec.transpose());
+    std::cout << sparse << std::endl;
 }
